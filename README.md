3a8c1402509b7fd906c57f7f0aa7491e5842c1a2
Chika <catherine.achukwu@gmail.com>
2019-08-15 16:23:09 -0400

    Changes: Do what it says function added, README updated

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # CLI-App
-LIRI is a _Language_ Interpretation and Recognition Interface. LIRI is a command line node app that takes in a song name and returns information for the song from spotify API database or movie names and returns information for the movie from OMBD API database. Requests to this app can be made from the terminal. To request a movie, enter `node liri.js movie-this '<movie name here>'`. To request a song enter '`node liri.js spotify-this-song '<song name here>'`'
+LIRI is a _Language_ Interpretation and Recognition Interface. LIRI is a command line node app that takes in a song name and returns information for the song from spotify API database or movie names and returns information for the movie from OMBD API database. Requests to this app can be made from the terminal. To request a movie, enter `node liri.js movie-this '<movie name here>'`. To request a song enter '`node liri.js spotify-this-song '<song name here>'`'. All requests are logged in the log.js file on the folder
 This app was developed by Chika Achukwu
 
 To view this app in action you can:
-1. Click the following link https://drive.google.com/file/d/1E727HPLj9Ih71lnd3_d8wPVrezNw9tBR/view
-2. Click on the node.js OMBD and Spotify video.mp4 file in the folder
+1. Click the following link https://drive.google.com/file/d/1nJTaojug34FuOXO_7yOONfLzuuyje6Un/view?usp=sharing
+2. Click on the node.js OMBD and Spotify video.mp4 file in the folder (this mp4 video does not show the "do-what-it-says feature of the app but you can see this in the drive)
 3. Click on the node.js OMBD and Spotify Screenshot.png file in the folder
 
--- liri.js
+++ liri.js
@@ -1,49 +1,69 @@
 require("dotenv").config()
 var axios = require("axios");
+var fs = require('fs');
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify({
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+});
+
+let demand;
 
 if (process.argv[2] == "movie-this") {
-    let movieName = process.argv[3]
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-    // console.log(queryUrl);
-    axios.get(queryUrl).then(
-        function (response) {
-            // console.log(response);
-            console.log("The title of the movie is: " + response.data.Title)
-            console.log("This movie was released in: " + response.data.Year)
-            console.log("The IMDB Rating of the movie is: " + response.data.imdbRating)
-            // console.log("The Rotten Tomatoes Rating of this movie is:")
-            console.log("This movie was produced in: " + response.data.Country)
-            console.log("The language of this movie is: " + response.data.Language)
-            console.log("The plot of this movie is: " + response.data.Plot)
-            console.log("The actors in this movie are: " + response.data.Actors)
-        })
-        .catch(function (error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.log("---------------Data---------------");
-                console.log(error.response.data);
-                console.log("---------------Status---------------");
-                console.log(error.response.status);
-                console.log("---------------Status---------------");
-                console.log(error.response.headers);
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                console.log(error.request);
+    let type = typeof (process.argv[3])
+
+    if (type == "string") {
+        let songName = "";
+        let nodeArgs = process.argv;
+
+        for (let i = 3; i < nodeArgs.length; i++) {
+            if (i > 3 && i < nodeArgs.length) {
+                songName = songName + " " + nodeArgs[i];
+
             } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log("Error", error.message);
+                songName += nodeArgs[i];
             }
-            console.log(error.config);
-        });
+        }
+
+        songName = songName.toString()
+        var queryUrl = "http://www.omdbapi.com/?t=" + songName + "&y=&plot=short&apikey=trilogy";
+        // console.log(queryUrl);
+        axios.get(queryUrl).then(
+            function (response) {
+                // console.log(response);
+                console.log("The title of the movie is: " + response.data.Title)
+                console.log("This movie was released in: " + response.data.Year)
+                console.log("The IMDB Rating of the movie is: " + response.data.imdbRating)
+                // console.log("The Rotten Tomatoes Rating of this movie is:")
+                console.log("This movie was produced in: " + response.data.Country)
+                console.log("The language of this movie is: " + response.data.Language)
+                console.log("The plot of this movie is: " + response.data.Plot)
+                console.log("The actors in this movie are: " + response.data.Actors)
+            })
+            .catch(function (error) {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+            });
+        write(demand, songName)
+    }
 } else if (process.argv[2] == "spotify-this-song") {
 
-    var Spotify = require('node-spotify-api');
-    var spotify = new Spotify({
-        id: process.env.SPOTIFY_ID,
-        secret: process.env.SPOTIFY_SECRET
-    });
 
     let type = typeof (process.argv[3])
 
@@ -85,22 +105,46 @@ if (process.argv[2] == "movie-this") {
             console.log("The album for this song is " + rightArr.album.name)
             console.log("Click the following link to hear this song " + rightArr.external_urls.spotify)
         })
+        write(demand, songName)
 
 
 
     } else {
         let songName = "The Sign (US Album) [Remastered]"
-        spotify.search({ type: 'track', query: songName }, function (err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err);
-            }
-            console.log("This song was made by " + data.tracks.items[0].album.artists[0].name)
-            console.log("The name of the song is " + data.tracks.items[0].name)
-            console.log("The album for this song is " + data.tracks.items[0].album.name)
-            console.log("Click the following link to hear this song " + data.tracks.items[0].external_urls.spotify)
-        })
+        consoled(songName)
+        write(demand, songName)
+
     }
+} else if (process.argv[2] == "do-what-it-says") {
+    fs.readFile('random.txt', "utf8", function (err, data) {
+        let songName = data.split(",")[1]
+        consoled(songName)
+        write(demand, songName)
+
+
+
+    })
 }
+function consoled(songName) {
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log("This song was made by " + data.tracks.items[0].album.artists[0].name)
+        console.log("The name of the song is " + data.tracks.items[0].name)
+        console.log("The album for this song is " + data.tracks.items[0].album.name)
+        console.log("Click the following link to hear this song " + data.tracks.items[0].external_urls.spotify)
+    })
+}
+function write(demand, songName) {
+    demand = process.argv[2]
 
+    fs.appendFile("log.txt", "\n" + demand + " " + songName + "\n", function (err) {
+        if (err) {
+            return console.log(err);
+        }
 
+        console.log("The file was saved!");
+    });
+}
 
